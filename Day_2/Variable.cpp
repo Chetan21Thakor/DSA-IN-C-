694082954f1b4efeb7677649b1a7555e1e67d937
Chetan Thakor <thakorchetan6043@gmail.com>
2026-08-03 10:08:22 +0530

    2 day to complete

---
+++ Day_2/Variable.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+ 
+
+int main(){
+    int a=10;// this line tell to compiler take memory location give name that memory location to "a" and store integer value 10.
+    //? 10 is literal . Literal has fix value and meaning which can't change. just like constant. for example here 10 is literal which meaning always 10 and value is always equal to 10.
+    //? Name of variable,class,object,structure,union etc.. which used to identify them it's called identifier. 
+    // ? tip :"Always give meaningful name to a identifier."
+    // int b;
+    // b=5;
+    // int c,d;
+    // c=11,d=12;
+    // int e=6,f=8;
+    // int g,h,i=21;
+    // g=h=2;
+}
---
+++ Day_2/datatype.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age=20;
+    char grade='A';
+    bool isAdult=true;
+    double height=5.9;
+    float weight=70.5;
+
+    //* this are the different data types in C++ and their usage. 
+    // *This are the Primitives data types in C++. int(integer) store a integer value, char(character) character store single character, bool(boolean) boolean store a boolean value, double(double precision floating point) double store a double precision floating point value, float(single precision floating point) float store a single precision floating point value.
+}
